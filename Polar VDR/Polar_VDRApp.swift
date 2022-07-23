48848caa419b01a58719f00105a4feb3251f32a9
William Kamp <manimaul@gmail.com>
2022-07-23 15:17:26 -0700

    Initial Commit

---
+++ Polar VDR/Polar_VDRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polar_VDRApp.swift
+//  Polar VDR
+//
+//  Created by William Kamp on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Polar_VDRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
